7abd9a057536de4434209f935e19f70afe39d47a
ibalpinar <ilker@balpinar.com>
2023-12-20 01:46:22 +0300

    Add dindex and main.js for the dynamically created html page

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+<title>Ilker Balpinar</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/style.css" />
+*/
+
+/*
+      <div class="resumewrapper">
+         <header></header>
+         <div class="main">
+            <section class="content_wrapper"></section>
+            <aside class="skills_and_others"></aside>
+         </div>
+      </div>
+*/
+
