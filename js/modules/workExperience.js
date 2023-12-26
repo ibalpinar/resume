302edc048109bec430509476c88c3d684a75a954
ibalpinar <ilker@balpinar.com>
2023-12-26 23:29:41 +0300

    Add all work experience and company data container

--- js/modules/workExperience.js
+++ js/modules/workExperience.js
@@ -3,72 +3,76 @@ export const generateWorkExperience = function(workExperienceData){
 
    const workTitle = document.createElement("h3");
    workTitle.classList.add("title");
-   workTitle.textContent = workExperienceData.title.toUpperCase();
+   workTitle.textContent = workExperienceData.workExperienceTitle.toUpperCase();
    headerWork.insertAdjacentElement("beforeend", workTitle);
 
    // Start of Company List
 
-   const workContainer = document.createElement("div");
-   workContainer.classList.add("container");
-   headerWork.insertAdjacentElement("beforeend", workContainer);
+   workExperienceData.list.forEach((workExperience) => {
 
-   const workBox = document.createElement("div");
-   workBox.classList.add("box");
-   workContainer.insertAdjacentElement("beforeend", workBox);
+      const workContainer = document.createElement("div");
+      workContainer.classList.add("container");
+      headerWork.insertAdjacentElement("beforeend", workContainer);
 
-   const experience = document.createElement("article");
-   experience.classList.add("experience");
-   workContainer.insertAdjacentElement("beforeend", experience);
+      const workBox = document.createElement("div");
+      workBox.classList.add("box");
+      workContainer.insertAdjacentElement("beforeend", workBox);
 
-   const workHeader = document.createElement("header");
-   experience.insertAdjacentElement("beforeend", workHeader);
+      // Section of Company
+      const experience = document.createElement("article");
+      experience.classList.add("experience");
+      workContainer.insertAdjacentElement("beforeend", experience);
 
-   const position = document.createElement("h4");
-   position.classList.add("position");
-   position.textContent = workExperienceData.list[0].position;
-   workHeader.insertAdjacentElement("beforeend", position);
+      const workHeader = document.createElement("header");
+      experience.insertAdjacentElement("beforeend", workHeader);
 
-   const company = document.createElement("h4");
-   company.classList.add("company");
-   position.textContent = workExperienceData.list[0].company;
-   workHeader.insertAdjacentElement("beforeend", company);
+      const position = document.createElement("h4");
+      position.classList.add("position");
+      position.textContent = workExperience.position;
+      workHeader.insertAdjacentElement("beforeend", position);
 
-   const durationLocation = document.createElement("h5");
-   durationLocation.classList.add("duration_location");
-   workHeader.insertAdjacentElement("beforeend", durationLocation);
+      const company = document.createElement("h4");
+      company.classList.add("company");
+      company.textContent = workExperience.company;
+      workHeader.insertAdjacentElement("beforeend", company);
 
-   const duration = document.createElement("div");
-   duration.classList.add("duration");
-   duration.textContent = workExperienceData.list[0].start + " - " + workExperienceData.list[0].end;
-   durationLocation.insertAdjacentElement("beforeend", duration);
+      const durationLocation = document.createElement("h5");
+      durationLocation.classList.add("duration_location");
+      workHeader.insertAdjacentElement("beforeend", durationLocation);
 
-   const location = document.createElement("div");
-   location.classList.add("location");
-   location.textContent = workExperienceData.list[0].location;
-   durationLocation.insertAdjacentElement("beforeend", location);
+      const duration = document.createElement("div");
+      duration.classList.add("duration");
+      duration.textContent = workExperience.start + " - " + workExperience.end;
+      durationLocation.insertAdjacentElement("beforeend", duration);
 
+      const location = document.createElement("div");
+      location.classList.add("location");
+      location.textContent = workExperience.location;
+      durationLocation.insertAdjacentElement("beforeend", location);
 
-   /////////////////
-   const accomplishments = document.createElement("div");
-   accomplishments.classList.add("accomplishments");
-   experience.insertAdjacentElement("beforeend", accomplishments);
+      // Section of Accomplishment
+      const accomplishments = document.createElement("div");
+      accomplishments.classList.add("accomplishments");
+      experience.insertAdjacentElement("beforeend", accomplishments);
 
-   const accomplishmentTitle = document.createElement("h5");
-   accomplishmentTitle.classList.add("title");
-   accomplishments.insertAdjacentElement("beforeend", accomplishmentTitle);
+      const accomplishmentTitle = document.createElement("h5");
+      accomplishmentTitle.classList.add("title");
+      accomplishmentTitle.textContent = workExperienceData.accomplishmentTitle
+      accomplishments.insertAdjacentElement("beforeend", accomplishmentTitle);
 
-   const accomplishmentList = document.createElement("ul");
-   accomplishments.insertAdjacentElement("beforeend", accomplishmentList);
+      const accomplishmentList = document.createElement("ul");
+      accomplishments.insertAdjacentElement("beforeend", accomplishmentList);
 
-   const accomplishmentItem = document.createElement("li");
-   accomplishmentList.insertAdjacentElement("beforeend", accomplishmentItem);
+      // Start of Accomplishment List
+      workExperience.accomplishments.forEach((accomplishment) => {
+         const accomplishmentItem = document.createElement("li");
+         accomplishmentList.insertAdjacentElement("beforeend", accomplishmentItem);
 
-   // Start of Accomplishment List
-   const accomplishmentText = document.createElement("div");
-   accomplishmentText.textContent = workExperienceData.list[0].accomplishments[0];
-   accomplishmentItem.insertAdjacentElement("beforeend", accomplishmentText);
-   // end of Accomplishment List
-
+         const accomplishmentText = document.createElement("div");
+         accomplishmentText.textContent = accomplishment
+         accomplishmentItem.insertAdjacentElement("beforeend", accomplishmentText);
+      });
+      // end of Accomplishment List
+   });
    // End of Company List
-
 };
