50b6b4dcfd31c38a63970a1ac06088dc196749b9
ibalpinar <ilker@balpinar.com>
2023-12-27 00:26:19 +0300

    Add course section

--- js/modules/courses.js
+++ js/modules/courses.js
@@ -1,2 +1,40 @@
+/*
+<article class="courses">
+   <h3 class="title">COURSES</h3>
+   <div class="container">
+      <div class="name">CTO Leadership</div>
+      <div class="company">Kloia</div>
+   </div>
+   [LOOP]
+</article>
+*/
 export const generateCourses = function(coursesData, resumeConfiguration){
+   const othersAside = document.querySelector(".others");
+
+   const courses = document.createElement("article");
+   courses.classList.add("courses");
+   othersAside.insertAdjacentElement("beforeend", courses);
+
+   const coursesTitle = document.createElement("h3");
+   coursesTitle.classList.add("title");
+   coursesTitle.textContent = resumeConfiguration.coursesTitle.toUpperCase();
+   courses.insertAdjacentElement("beforeend", coursesTitle);
+
+   // Start of Courses List
+   coursesData.forEach((course) => {
+      const courseContainer = document.createElement("div");
+      courseContainer.classList.add("container");
+      courses.insertAdjacentElement("beforeend", courseContainer);
+
+      const courseName = document.createElement("div");
+      courseName.classList.add("name");
+      courseName.textContent = course.name;
+      courseContainer.insertAdjacentElement("beforeend", courseName);
+
+      const courseCompany = document.createElement("div");
+      courseCompany.classList.add("company");
+      courseCompany.textContent = course.company;
+      courseContainer.insertAdjacentElement("beforeend", courseCompany);
+   });
+   // end of Courses List
 };
